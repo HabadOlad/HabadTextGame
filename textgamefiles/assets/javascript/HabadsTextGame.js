3ccfdc4eebaa23aa6cd1cfc9c59bb82305325dd3
Habad Olad <olad.hab@gmail.com>
2026-06-12 03:31:22 +0100

    feat:cleaning up html and js, adding a new story

--- textgamefiles/assets/javascript/HabadsTextGame.js
+++ textgamefiles/assets/javascript/HabadsTextGame.js
@@ -1,22 +1,15 @@
-// const playerName = prompt("what is your name?:");
-
 const statusBar = document.querySelector(".location");
 const inputEl = document.getElementById("input");
 const outputEl = document.getElementById("output");
+const nameprompt = document.getElementById("nameprompt");
 
 // Initial description
+
 outputEl.innerHTML += `<div class="prompt">$></div><div>Habad's Interactive Fiction,Copyright (c) 2025,
 Habadcorp, Inc. All rights reserved.
 Habad's Interactive fiction is a registered trademark of
 HabadCorp, Inc.</div>`;
-outputEl.innerHTML += `<div class="prompt">$></div><div>Welcome to Habad's Interactive Fiction, ${playerName}! The commands are: look up and look down, north/ south/ east/ west</div>`;
-outputEl.innerHTML += `<div class="prompt">$></div><div>So hero: ${playerName}! Can you find the treasure before you get chopped up by the zombie butcher?</div>`;
-outputEl.innerHTML += `quick tip...always look up first!`;
+outputEl.innerHTML += `<div class="prompt">$></div><div>Welcome to Habad's Interactive Fiction, adventuerer !</div>`;
+outputEl.innerHTML += `<div class="prompt">$></div><div>You have somehow found this dingy degraded battered old wooden door, it's your chance too escape the enemies relentelss soldiers who've been tracking you. However, this building may have more too it than you thought adventurer!.....Do you dare enter?</div>`;
 
 statusBar.innerHTML += `location placeholder for now`;
-
-// inputEl.addEventListener("keydown", (event) => {
-
-//   if(event.key == "Enter")
-
-//   });
