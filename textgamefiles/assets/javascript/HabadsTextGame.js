d1d48f133262939a261d486cbdf99616d92860ef
Habad Olad <olad.hab@gmail.com>
2026-06-16 04:29:30 +0100

    feat:cleaning up more html,css and js, also adding more storyline

--- textgamefiles/assets/javascript/HabadsTextGame.js
+++ textgamefiles/assets/javascript/HabadsTextGame.js
@@ -1,10 +1,12 @@
-const statusBar = document.querySelector(".location");
+const locationBar = document.querySelector(".location");
+const statsBar = document.querySelector(".stats");
 const inputEl = document.getElementById("input");
 const outputEl = document.getElementById("output");
 const nameprompt = document.getElementById("nameprompt");
 
 //status bar = location and updates of the location
-statusBar.innerHTML += `location placeholder for now`;
+locationBar.innerHTML = `The Eerie Estate - Front Gate`;
+statsBar.innerHTML = `Score: 0 / Moves: 0`; // Pushes perfectly to the right!
 
 // Initial output for the text
 outputEl.innerHTML += `<div>Habad's Interactive Fiction,Copyright (c) 2025,
@@ -24,4 +26,4 @@ outputEl.innerHTML += `<pre class="welcome-art">${`
 outputEl.innerHTML += `<br />`;
 outputEl.innerHTML += `<div>You have somehow found this dingy degraded battered old wooden door, it's your chance to escape the enemies relentless soldiers who've been tracking you. However, this building may have more to it than you thought adventurer!.....Do you dare enter?</div>`;
 outputEl.innerHTML += `<br />`;
-nameprompt.innerHTML += `What is your name?....`;
+//nameprompt.innerHTML = `$>`;
